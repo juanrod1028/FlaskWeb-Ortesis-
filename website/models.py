fb1e699d25f0e0ebbbc6683edfdab1fb62d9e877
juanrod1028 <juanrodriguez1028@gmail.com>
2021-05-04 13:22:06 -0500

    master

---
+++ website/models.py
@@ -0,0 +1,45 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
+    productos = db.relationship('Producto')
+    carrito = db.relationship('Carrito')
+
+class Producto(db.Model):
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    precio = db.Column(db.Integer, default=0)
+    descripcion = db.Column(db.String(255))
+    imagen = db.Column(db.String(255))
+    categoria = db.Column(db.String(255), default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    carrito = db.relationship('Carrito')
+    
+
+class MiProducto(db.Model):
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    precio = db.Column(db.Integer, default=0)
+    descripcion = db.Column(db.String(255))
+    imagen = db.Column(db.String(255))
+    categoria = db.Column(db.String(255), default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Carrito(db.Model):
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    producto_id = db.Column(db.Integer, db.ForeignKey('producto.id'))
